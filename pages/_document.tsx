01bfb5857483f9164096b352dd98f544160e8a6b
aha-oretama <sekine_y_529@msn.com>
2020-12-27 12:14:28 +0900

    add Head, CustomDocument

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, {DocumentContext, Html, Head, Main, NextScript} from "next/document";
+import React from "react";
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {...initialProps};
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang='en'>
+        <Head>
+        </Head>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </Html>
+    )
+  }
+}
