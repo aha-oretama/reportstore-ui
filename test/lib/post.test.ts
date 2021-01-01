6d387aeefd4018ffa00e3d049a7a3b8d8e6cde97
aha-oretama <sekine_y_529@msn.com>
2021-01-01 12:49:06 +0900

    change id.tsx, remove unnecessary libraries

--- test/lib/post.test.ts
+++
@@ -1,8 +0,0 @@
-import { getAllPostIds } from '../../lib/posts';
-
-it('getAllPostIds should return ids', () => {
-  const ids = getAllPostIds();
-  expect(ids).toHaveLength(2);
-  expect(ids[0].params.id).toBe('pre-rendering');
-  expect(ids[1].params.id).toBe('ssg-ssr');
-});
