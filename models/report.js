320bb25cf9923746bbb477b1169d66e4be0d3f00
aha-oretama <sekine_y_529@msn.com>
2021-01-04 16:22:18 +0900

    fix lint-staged

--- models/report.js
+++ models/report.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Report.hasMany(models.suite)
+      Report.hasMany(models.suite);
     }
   }
 
