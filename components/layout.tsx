9fcebf182c6d6743415609d5bdea6f1e4ee1ee6a
aha-oretama <sekine_y_529@msn.com>
2021-01-09 17:56:27 +0900

    introduce auth0

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,9 @@
 import Head from 'next/head';
 import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import React from 'react';
+import Header from './header';
 
-const name = 'aha-oretama';
 export const siteTitle = 'Practice Next.js';
 
 export default function Layout({
@@ -31,35 +30,7 @@ export default function Layout({
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/profile.jpg"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <img
-                  src="/images/profile.jpg"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
+      <Header />
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app';
 import '../styles/global.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Auth0Provider
+      domain="dev--testserve.us.auth0.com"
+      clientId="o3cb19lTn5UddDUqd5RgpFaR5RwoCcXX"
+      redirectUri="http://localhost:3000" //{window.location.origin}
+    >
+      <Component {...pageProps} />;
+    </Auth0Provider>
+  );
 };
 
 export default App;
