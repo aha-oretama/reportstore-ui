53d89f6e1752bffcf0044cb6422737c5d377521a
aha-oretama <sekine_y_529@msn.com>
2021-02-04 23:27:30 +0900

    use Link in repositoryId page

--- pages/gh/[repositoryId].tsx
+++ pages/gh/[repositoryId].tsx
@@ -6,6 +6,7 @@ import Layout from '../../components/layout';
 import Head from 'next/head';
 import { useRepository } from '../../hooks/git/useRepositories';
 import moment from 'moment';
+import Link from 'next/link';
 
 const GetRepositoryId: React.FunctionComponent = () => {
   const { user, loading } = useFetchUser();
@@ -36,59 +37,61 @@ const GetRepositoryId: React.FunctionComponent = () => {
         <ul className="divide-y divide-gray-200">
           {testsData.map((testData) => (
             <li key={testData.id}>
-              <div className="block hover:bg-blue-50">
-                <div className="px-4 py-4 sm:px-6">
-                  <div className="flex items-center justify-between">
-                    <div className="flex justify-start">
-                      {/* FIXME: commit user should be show */}
-                      <img
-                        src={repository.owner.avatar_url}
-                        alt="My profile icon"
-                        className="w-12 h-12 rounded-full"
-                      />
-                      <div className="px-2 py-0.5 sm:px-6">
-                        <p className="text-gray-700 truncate">
-                          {testData.name}
-                        </p>
-                        <div className="mt-2 sm:flex sm:justify-between">
-                          <div className="sm:flex">
-                            <p className="flex items-center text-sm font-light text-gray-500 justify-between">
-                              <time className='mr-2' dateTime="2020-01-07">
-                                {moment(testData.createdAt).format()}
-                              </time>
-                              <div className='flex items-center'>
-                                <svg className="mr-0.5 stroke-current text-gray-500" fill="none" height="12"
-                                     stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                     stroke-width="2" viewBox="0 0 24 24" width="12" xmlns="http://www.w3.org/2000/svg">
-                                  <circle cx="12" cy="12" r="10">
-                                  </circle>
-                                  <polyline points="12 6 12 12 16 14">
-                                  </polyline>
-                                </svg>
-                                {testData.time}
-                              </div>
-                            </p>
+              <Link href={`/gh/${repositoryId}/tests/${testData.id}`}>
+                <a className="block hover:bg-blue-50">
+                  <div className="px-4 py-4 sm:px-6">
+                    <div className="flex items-center justify-between">
+                      <div className="flex justify-start">
+                        {/* FIXME: commit user should be show */}
+                        <img
+                          src={repository.owner.avatar_url}
+                          alt="My profile icon"
+                          className="w-12 h-12 rounded-full"
+                        />
+                        <div className="px-2 py-0.5 sm:px-6">
+                          <p className="text-gray-700 truncate">
+                            {testData.name}
+                          </p>
+                          <div className="mt-2 sm:flex sm:justify-between">
+                            <div className="sm:flex">
+                              <p className="flex items-center text-sm font-light text-gray-500 justify-between">
+                                <time className='mr-2' dateTime="2020-01-07">
+                                  {moment(testData.createdAt).format()}
+                                </time>
+                                <div className='flex items-center'>
+                                  <svg className="mr-0.5 stroke-current text-gray-500" fill="none" height="12"
+                                       stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                       stroke-width="2" viewBox="0 0 24 24" width="12" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="12" cy="12" r="10">
+                                    </circle>
+                                    <polyline points="12 6 12 12 16 14">
+                                    </polyline>
+                                  </svg>
+                                  {testData.time}
+                                </div>
+                              </p>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="ml-2 flex-shrink-0 flex items-center">
+                      <div className="ml-2 flex-shrink-0 flex items-center">
                       <span className='mr-2'>
                         {`${(testData.tests - testData.failures + testData.errors)}/${testData.tests}`}
                       </span>
-                      {testData.errors > 0 || testData.failures > 0 ? (
-                        <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
-                          Failure
-                        </p>
-                      ) : (
-                        <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                          Success
-                        </p>
-                      )}
+                        {testData.errors > 0 || testData.failures > 0 ? (
+                          <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                            Failure
+                          </p>
+                        ) : (
+                          <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                            Success
+                          </p>
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
+                </a>
+              </Link>
             </li>
           ))}
         </ul>
