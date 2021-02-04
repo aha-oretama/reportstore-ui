9c9c9c64e7691d599a2979ae2a48579a5c026d89
aha-oretama <sekine_y_529@msn.com>
2021-02-04 23:31:29 +0900

    fix lint

--- pages/gh/[repositoryId].tsx
+++ pages/gh/[repositoryId].tsx
@@ -60,8 +60,8 @@ const GetRepositoryId: React.FunctionComponent = () => {
                                 </time>
                                 <div className='flex items-center'>
                                   <svg className="mr-0.5 stroke-current text-gray-500" fill="none" height="12"
-                                       stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                       stroke-width="2" viewBox="0 0 24 24" width="12" xmlns="http://www.w3.org/2000/svg">
+                                       stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                       strokeWidth="2" viewBox="0 0 24 24" width="12" xmlns="http://www.w3.org/2000/svg">
                                     <circle cx="12" cy="12" r="10">
                                     </circle>
                                     <polyline points="12 6 12 12 16 14">
