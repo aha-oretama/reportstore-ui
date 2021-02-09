e9837a1055c3fe73e89b3f2f41a5f9289d2cd01a
aha-oretama <sekine_y_529@msn.com>
2021-02-10 00:20:11 +0900

    fix lints for _app, _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,9 +6,10 @@ import Document, {
   NextScript,
 } from 'next/document';
 import React from 'react';
+import {DocumentInitialProps} from "next/dist/next-server/lib/utils";
 
 export default class CustomDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
